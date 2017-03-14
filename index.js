0e9a386d9a5c168da33a2b311b187b203fb501c3
sam201994 <smritigoel93@gmail.com>
2017-03-13 17:25:47 -0700

    does not handle exception move. needs to input right coordinates

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const readline = require('readline');
+var prompt = require('prompt');
 class Player {
 
 	constructor(name, symbol){
@@ -25,7 +26,7 @@ class Board{
 			
 			var temp =[]
 			for(var j =0;j<3; j++) {
-				temp.push("--")
+				temp.push("*")
 			}
 			this.board.push(temp);
 		}
@@ -37,7 +38,7 @@ class Board{
 	gameStatus() {
 		for(var i = 0; i < 3; i++){
 
-			if(this.board[i][0] != "--" && this.board[i][0] == board[i][1] && board[i][1] == board[i][2]){
+			if(this.board[i][0] != "*" && this.board[i][0] == this.board[i][1] && this.board[i][1] == this.board[i][2]){
 				if(this.board[i][0] == this.player1Symbol){
 					return this.PLAYER1WON;
 				}
@@ -48,7 +49,7 @@ class Board{
 		}
 
 		for(var i = 0; i < 3; i++){
-			if(this.board[0][i] != "--" && this.board[0][i] == this.board[1][i] && this.board[1][i] == this.board[2][i]){
+			if(this.board[0][i] != "*" && this.board[0][i] == this.board[1][i] && this.board[1][i] == this.board[2][i]){
 				if(board[0][i] == this.player1Symbol){
 					return this.PLAYER1WON;
 				}
@@ -57,8 +58,8 @@ class Board{
 				}
 			}
 		}
-		if(this.board[0][0] != '--' && this.board[0][0] == this.board[1][1] && this.board[1][1] == this.board[2][2]){
-			if(this.board[0][0] == player1Symbol){
+		if(this.board[0][0] != '*' && this.board[0][0] == this.board[1][1] && this.board[1][1] == this.board[2][2]){
+			if(this.board[0][0] == this.player1Symbol){
 				return this.PLAYER1WON;
 			}
 			else{
@@ -66,7 +67,7 @@ class Board{
 			}
 		}
 
-		if(this.board[0][2] != '--' && this.board[0][2] == this.board[1][1] && this.board[1][1] == this.board[2][0]){
+		if(this.board[0][2] != '*' && this.board[0][2] == this.board[1][1] && this.board[1][1] == this.board[2][0]){
 			if(this.board[0][2] == this.player1Symbol){
 				return this.PLAYER1WON;
 			}
@@ -77,7 +78,7 @@ class Board{
 
 		for(var i = 0; i < 3; i++){
 			for(var j = 0; j < 3; j++ ){
-				if(this.board[i][j] == '--'){
+				if(this.board[i][j] == '*'){
 					return this.INCOMPLETE;
 				}
 			}
@@ -89,10 +90,20 @@ class Board{
 		x = parseInt(x);
 		y = parseInt(y);
 		this.board[x][y] = symbol;
+		this.printGame();
+	
 	}
 	printGame() {
 		console.log("PRINT BOARD");
-		console.log("\n",this.board);
+		var finalAns="";
+		for(var i = 0; i<3; i++){
+			var str="|"
+			for(var j=0 ;j<3; j++){
+				str = str+this.board[i][j]+"|";
+			}
+			finalAns = finalAns + str +"\n";
+		}
+		console.log(finalAns);
 	}
 
 }
@@ -122,87 +133,62 @@ class TicTacToe {
 		console.log("here is player 2: ",player2);
 
 		var board = new Board(player1.symbol, player2.symbol);
-
 		var player1Turn = true; 
-		var done;
-		while(board.gameStatus() ===  board.INCOMPLETE){
 
+		function ola(){
 			if(player1Turn){
 			
-					var x;
-					var y;
-					console.log("Player 1 turn !");
+				var x;
+				var y;
 
-					const rl = readline.createInterface({
-					  input: process.stdin,
-					  output: process.stdout
-					});
-
-					rl.question('Enter X: ', (answer) => {
-
-					  x = answer;
-					  rl.close();
-
-						const r2 = readline.createInterface({
-						  input: process.stdin,
-						  output: process.stdout
-						});
-
-						r2.question('Enter Y: ', (answer) => {
-						  y = answer;
-						  board.move(player1.symbol,x,y);
-						  player1Turn = false;  
-						  r2.close();
-						});
-
-					  
-					});
+				prompt.start();
+				prompt.get(['x', 'y'], function (err, result) { 
+					console.log("inside call back for player 1: ", result);
+				board.move(player1.symbol, result.x, result.y);
+				   player1Turn = false;	
+				   if(board.gameStatus() === board.PLAYER1WON){
+						console.log(player1.name+" wins !!!");
+						return;
+					}
+					else if(board.gameStatus() === board.PLAYER2WON){
+						console.log(player2.name+" wins !!!");
+						return;
+					}
+					else if (board.gameStatus() === board.DRAW){
+						console.log("Its a Draw !!");
+						return;
+					}
+				   ola();	
+				});	
 				
 			}
 			else {
+
 				var x;
 				var y;
 				console.log("Player 2 turn !");
 
-				const rl = readline.createInterface({
-				  input: process.stdin,
-				  output: process.stdout
-				});
-
-				rl.question('Enter X: ', (answer) => {
-				  x = answer;
-
-					const r2 = readline.createInterface({
-					  input: process.stdin,
-					  output: process.stdout
-					});
-					r2.question('Enter Y: ', (answer) => {
-					  y = answer;
-					  board.move(player2.symbol,x,y);
-					  player1Turn = true;
-					  r2.close();
-					});
-
-				  rl.close();
+				prompt.start();
+				prompt.get(['x', 'y'], function (err, result) { 
+				board.move(player2.symbol, result.x, result.y);
+				   player1Turn = true;
+				   if(board.gameStatus() == board.PLAYER1WON){
+						console.log(player1.name+" wins !!!");
+					}
+					else if(board.gameStatus() == board.PLAYER2WON){
+						console.log(player2.name+" wins !!!");
+					}
+					else if(board.gameStatus() === board.DRAW){
+						console.log("Its a Draw !!");
+					}
+				   ola();		
 				});	
-				
-				
 			}
 
-			board.printGame();
-		}	
-		
-		
-		if(board.gameStatus() == board.PLAYER1WON){
-			console.log(player1.name+" wins !!!");
+			
+
 		}
-		else if(board.gameStatus() == board.PLAYER2WON){
-			console.log(player2.name+" wins !!!");
-		}
-		else{
-			console.log("Its a Draw !!");
-		}
-	
+		ola();
 
 	}
 
@@ -212,35 +198,9 @@ class TicTacToe {
 		var name;
 		var symbol;
 		if(num === 1)
-			return new Player("Smriti", "X");
-		return new Player("Vrishti", "O");
-
-		console.log("ENTER DETAILS FOR ", num, " PLAYER");
-
-
-		const rl = readline.createInterface({
-		  input: process.stdin,
-		  output: process.stdout
-		});
-
-		rl.question('Enter Name: ', (n) => {
-		  name = n; 
-		  rl.close();
-
-			const r2 =readline.createInterface({
-			  input: process.stdin,
-			  output: process.stdout
-			});
-			
-			r2.question('Enter symbol: ', (s) => {
-			  symbol = s;
-			 
-
-			  var p = new Player("smriti", "x");
-			  r2.close();
-			  return p;
-			});
-		});
+			return new Player("Player1", "X");
+		return new Player("Player2", "O");
+	
 	}
 }
 var t = new TicTacToe();
